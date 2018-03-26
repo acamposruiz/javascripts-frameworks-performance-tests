c0d2f1b7e403beaa1e320d11672542464ee8ff38
Antonio Campos <acamposruiz@gmail.com>
2018-03-26 21:24:36 +0200

    React - Second counter

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-import Timer from 'easytimer.js';
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -13,11 +12,11 @@ class App extends Component {
       mockData: [], // Where the list of elements will be store
       isToggleOn: false, // On/Off state indicator
       step: 0, // Iteration step
-      secondsCounter: "0"
+      secondsCounter: 0,
+      timer: undefined
     };
     this.switchState = this.switchState.bind(this);
     this.clear = this.clear.bind(this);
-    this.timer = new Timer();
   }
 
   /* Here we manage switching from initial state to running state (and the way back) when the application starts to generate data */
@@ -27,16 +26,16 @@ class App extends Component {
     if (this.state.int) {
       clearInterval(this.state.int);
       newState.int = undefined;
-      newState.secondsCounter = "0";
-      this.timer = new Timer();
+      clearInterval(this.state.timer);
     } else {
-      this.timer.start();
+      newState.timer = setInterval(() => {
+        this.state.secondsCounter += 1;
+      }, 1000);
       /* If the application isn't running it starts to generate data */
       newState.int = setInterval(() => {
         if (newState.step >= 219) this.clear(false);
         newState.step++;
         this.setState({
-          secondsCounter: this.timer.getTimeValues().seconds.toString(),
           step: newState.step,
           mockData: _.shuffle(_.range(newState.step * 10).map((element, index) => {
             return {style: this.divStyle(), content: index};
@@ -52,14 +51,16 @@ class App extends Component {
     const newState = {mockData: [], step: 0};
     if (this.state.int) {
       clearInterval(this.state.int);
-      newState.int = undefined;
-      newState.isToggleOn = false;
     }
     if (manual) {
+      clearInterval(this.state.timer);
       newState.secondsCounter = 0;
-      this.timer = new Timer();
     }
-    this.setState(newState);
+    this.setState({
+      ...newState,
+      isToggleOn: false,
+      int: undefined
+    });
   }
 
   divStyle() {
