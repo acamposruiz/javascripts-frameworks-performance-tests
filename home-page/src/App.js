a3381cadfae0ab4e612d477928317919e0fee08e
Antonio Campos <acamposruiz@gmail.com>
2018-04-20 19:52:15 +0200

    HP - First HTML composition

--- home-page/src/App.js
+++ home-page/src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      frameworks: [
+        {
+          name: "ReactJs",
+          path: "react"
+        },
+        {
+          name: "Angular",
+          path: "angular"
+        },
+        {
+          name: "VueJs",
+          path: "vue"
+        }
+      ]
+    };
+  }
+
   render() {
+    const sm = "12";
+    const md = parseInt(12/this.state.frameworks.length, 10);
+
     return (
       <div className="">
         <div className="row">
-          <div  className="title col-sm-12">
+          <div  className="title col-sm-12 col-md-12">
             <h1>Test yourself a Js framwork</h1>
           </div>
-          <a href="react" className="react col-sm">
-            React
-          </a>
-          <a href="angular" className="angular col-sm">
-            Angular
-          </a>
-          <a href="angular" className="vue col-sm">
-            Vue
-          </a>
+        </div>
+        <div className="row app-container">
+          {this.state.frameworks.map(function (fm, i) {
+            return <a key={i} href={fm.path} className={`${fm.name.toLowerCase()} col-sm-${sm} col-md-${md}`}>
+              {fm.name}
+            </a>
+          })}
         </div>
       </div>
     );
