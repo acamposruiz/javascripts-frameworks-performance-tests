5a8543abbc5184e36297ea39574e3d2c37586287
Antonio Campos <acamposruiz@gmail.com>
2018-04-23 18:09:54 +0200

    HP - icon name

--- home-page/src/App.js
+++ home-page/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import logoReact from './logo.svg';
+import logoReact from './react.svg';
 import logoAngular from './angular.svg';
 import logoGithub from './github-icon.svg';
 import './App.css';
