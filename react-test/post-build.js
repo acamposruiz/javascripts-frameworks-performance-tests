004f28b86a0ef269b91bd7e5773dd168bb68e9c9
Antonio Campos <acamposruiz@gmail.com>
2018-04-24 07:52:50 +0200

    React - build - remove old build script

--- react-test/post-build.js
+++ react-test/post-build.js
@@ -1,6 +1,13 @@
 const replace = require('replace-in-file');
 const path = require('path');
 
+var fs = require('fs')
+  , _ = require('underscore');
+
+var buildend = path.resolve(__dirname, "../builds/react");
+
+
+/*
 const options = [
   {
     //Single file
@@ -14,8 +21,8 @@ const options = [
     //Single file
     files: [
       path.resolve(__dirname, 'build/index.html'),
-      path.resolve(__dirname, 'build/static/js/*.js'),
-      path.resolve(__dirname, 'build/static/css/*.css'),
+      path.resolve(__dirname, 'build/static/js/!*.js'),
+      path.resolve(__dirname, 'build/static/css/!*.css'),
       path.resolve(__dirname, 'build/service-worker.js')
     ],
 
@@ -34,3 +41,21 @@ options.forEach(option => {
       console.error('Error occurred:', error);
     });
 });
+*/
+
+
+function rmDir(dirPath) {
+  try { var files = fs.readdirSync(dirPath); }
+  catch(e) { return; }
+  if (files.length > 0)
+    for (var i = 0; i < files.length; i++) {
+      var filePath = dirPath + '/' + files[i];
+      if (fs.statSync(filePath).isFile())
+        fs.unlinkSync(filePath);
+      else
+        rmDir(filePath);
+    }
+  if (buildend !== dirPath) fs.rmdirSync(dirPath);
+};
+
+rmDir(buildend);
