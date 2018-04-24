cd2a092602f1c2954c9f01a8c43373ccb55f026d
Antonio Campos <acamposruiz@gmail.com>
2018-04-24 20:45:22 +0200

    General - refactor post script build

---
+++ home-page/post-build.js
@@ -0,0 +1,44 @@
+const postBuild = require('../commons/post-build');
+const path = require('path');
+
+const options = [
+  {
+    //Single file
+    files: path.resolve(__dirname, 'build/asset-manifest.json'),
+
+    //Replacement to make (string or regex)
+    from: /: "static/g,
+    to: ': "/js-frameworks-tests/builds/react/static',
+  },
+  {
+    //Single file
+    files: [
+      path.resolve(__dirname, 'build/index.html'),
+      path.resolve(__dirname, 'build/static/js/*.js'),
+      path.resolve(__dirname, 'build/static/css/*.css'),
+      path.resolve(__dirname, 'build/service-worker.js')
+    ],
+
+    //Replacement to make (string or regex)
+    from: /static/g,
+    to: 'js-frameworks-tests/builds/react/static',
+  },
+  {
+    //Single file
+    files: [
+      path.resolve(__dirname, 'build/static/js/*.js')
+    ],
+
+    //Replacement to make (string or regex)
+    from: /\.\.\/static/g,
+    to: 'js-frameworks-tests/builds/react/static',
+  }
+];
+
+const paths = {
+  rootPath: __dirname,
+  buildEndPath: "../builds/home",
+  buildOriginPath: "build"
+};
+
+postBuild(paths, options);
