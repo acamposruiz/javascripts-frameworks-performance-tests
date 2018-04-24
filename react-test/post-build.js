36a6a0cda7b363c98a2f3f91507363eb8770f116
Antonio Campos <acamposruiz@gmail.com>
2018-04-24 07:43:37 +0200

    React - npm - post-build

---
+++ react-test/post-build.js
@@ -0,0 +1,36 @@
+const replace = require('replace-in-file');
+const path = require('path');
+
+const options = [
+  {
+    //Single file
+    files: path.resolve(__dirname, 'build/asset-manifest.json'),
+
+    //Replacement to make (string or regex)
+    from: /: "static/g,
+    to: ': "/js-frameworks-tests/builds/react/static',
+  },
+  {
+    //Single file
+    files: [
+      path.resolve(__dirname, 'build/index.html'),
+      path.resolve(__dirname, 'build/static/js/*.js'),
+      path.resolve(__dirname, 'build/static/css/*.css'),
+      path.resolve(__dirname, 'build/service-worker.js')
+    ],
+
+    //Replacement to make (string or regex)
+    from: /static/g,
+    to: 'js-frameworks-tests/builds/react/static',
+  }
+];
+
+options.forEach(option => {
+  replace(option)
+    .then(changedFiles => {
+      console.log('Modified files:', changedFiles.join(', '));
+    })
+    .catch(error => {
+      console.error('Error occurred:', error);
+    });
+});
